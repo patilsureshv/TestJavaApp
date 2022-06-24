1f244bd3a458279202a46be5532f43228d1180a7
Suresh Patil <suresh_Patil@persistent.com>
2022-06-24 17:14:55 +0530

    test commit

---
+++ src/main/java/com/pack/test/SpringAzurTestAppApplication.java
@@ -0,0 +1,23 @@
+package com.pack.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@ComponentScan("com.pack.test")
+@RestController
+public class SpringAzurTestAppApplication {
+	@GetMapping("/message")
+	public String messgae()
+	{
+		return "Welcome to Spring boot with Azur Integration!...";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzurTestAppApplication.class, args);
+	}
+
+}
